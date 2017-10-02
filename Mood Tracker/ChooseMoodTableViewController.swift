b78398d8d7454d65796cfd754199e3ab7636ae4f
Phyllis Wong <wong.phyllism@gmail.com>
2017-10-01 17:35:35 -0700

    updated moods and added button

---
+++ Mood Tracker/ChooseMoodTableViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ChooseMoodTableViewController.swift
+//  Mood Tracker
+//
+//  Created by djchai on 9/29/17.
+//  Copyright © 2017 djchai. All rights reserved.
+//
+
+import UIKit
+
+class ChooseMoodTableViewController: UITableViewController {
+
+    let moods: [Mood] = [
+        .happy,
+        .neutral,
+        .sad,
+        .angry
+    ]
+    
+    var selectedFriend: Friend?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return moods.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "moodsCell", for: indexPath)
+
+        cell.textLabel?.text = moods[indexPath.row].rawValue
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let selectedMood = moods[indexPath.row]
+        
+        selectedFriend?.mood = selectedMood
+        
+    }
+ 
+
+}
---
+++ Mood Tracker/ListFriendsTableViewController.swift
@@ -0,0 +1,101 @@
+//
+//  ListFriendsTableViewController.swift
+//  Mood Tracker
+//
+//  Created by djchai on 9/29/17.
+//  Copyright © 2017 djchai. All rights reserved.
+//
+
+import UIKit
+
+struct Friend {
+    let name: String
+    var mood: Mood?
+}
+
+enum Mood: String {
+    case happy = "😀"
+    case neutral = "😐"
+    case sad = "🙁"
+    case angry = "😡"
+}
+
+class ListFriendsTableViewController: UITableViewController {
+
+    var friends: [Friend] = [
+        Friend(name: "Ross", mood: nil),
+        Friend(name: "Rachel", mood: nil),
+        Friend(name: "Monica", mood: nil),
+        Friend(name: "Chandler", mood: nil),
+        Friend(name: "Pheobe", mood: nil),
+        Friend(name: "Joey", mood: nil)
+        
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return friends.count
+    }
+
+    // button to select friend
+    @IBAction func SelectFriend(_ sender: Any) {
+        // when pressed, goes to the moods view controller
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "friendsCell", for: indexPath)
+
+        let friend = friends[indexPath.row]
+        
+        
+        cell.textLabel?.text = "\(friend.name) \(friend.mood?.rawValue ?? "")"
+
+        return cell
+    }
+    
+    @IBAction func addPressed(_ sender: Any) {
+        
+        // Add your friend here
+    }
+    
+    // Tableview delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let selectedFriend = friends[indexPath.row]
+        
+        goToMoodChooserViewController(selectedFriend: selectedFriend)
+        
+    }
+    
+    func goToMoodChooserViewController(selectedFriend: Friend) {
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        guard let chooseMoodTableViewController = storyboard.instantiateViewController(withIdentifier: "ChooseMoodTableViewController") as? ChooseMoodTableViewController
+            else {return}
+        
+        
+        chooseMoodTableViewController.selectedFriend = selectedFriend
+        
+        // TODO: Make sure to set the delegate on the ChooseMoodTableViewController
+     
+        
+    self.navigationController?.pushViewController(chooseMoodTableViewController, animated: true)
+        
+    }
+    
+
+}
